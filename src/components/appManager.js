112d40e262924303828a483e6df55e16b769f06c
jordygg36 <jordygalind0413@gmail.com>
2024-11-25 09:25:41 -0600

    Update appManager.js

--- src/components/appManager.js
+++ src/components/appManager.js
@@ -57,7 +57,7 @@ class AppManager {
     }
 
     runService = async () => {
-        const thisServicePort = "this.#runningConfType.service.port";
+        const thisServicePort = "https://backend-o3wd.onrender.com";
         await this.#appExpress.listen(thisServicePort, () => {
             console.log(`AppManager is ready on ${thisServicePort}`);
         });
