84fe7077710883af5e33e9da5fb4bd29363d0729
mabel2828 <maribelhernandezsol2828@gmail.com>
2024-11-29 08:29:48 -0600

    Update usuarioControllers.js

--- src/controllers/usuarioControllers.js
+++ src/controllers/usuarioControllers.js
@@ -11,7 +11,7 @@ class usuarioControllers {
     fetchUsers = async (req, resp) => {
         // let data = await usuarioModel.fetchUsersAll();
         let data = [{ nombre:"Mabel", apellido:"Styles", direccion:"Junio 13", email:"mabel2828@gmail.com", contrasena:"2828"},
-                    nombre:"Ulises", apellido:"Styles", direccion:"Julio 13", email:"yonibravo@gmail.com", contrasena:"5656"}]
+                    {nombre:"Ulises", apellido:"Styles", direccion:"Julio 13", email:"yonibravo@gmail.com", contrasena:"5656"}]
         resp.status(200).json(data);
     }
 
